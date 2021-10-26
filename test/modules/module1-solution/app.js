4b3b089f57ae18ec317d04d88e11f9c1e9b8a09b
Faizal <faizal@ganpat-student.com>
2021-10-27 00:01:50 +0530

    This is my modules-1 solution

---
+++ test/modules/module1-solution/app.js
@@ -0,0 +1,26 @@
+(function() { 
+
+    angular.module('Module1' , [])
+    .controller('countController' , countController);
+
+    countController.$inject = ['$scope'];
+
+    function countController($scope) {
+        $scope.input = "hello , my , name , is , faizal";
+
+        $scope.Check = function () {
+            var count = $scope.input.split(",");
+            $scope.message;
+
+            if($scope.input == " " || $scope.input == "" || $scope.input == null){
+                $scope.message = "Please enter data first!!";
+            }
+            else if(count.length > 0 && count.length < 4) {
+                $scope.message = "Enjoy!";
+            }
+            else if(count.length >= 4){
+                $scope.message = "Too much!";
+            }
+        }
+    }
+})();
